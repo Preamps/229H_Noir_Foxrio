53413351fd57de170f8dfbfe8ca6af173dfe4146
Preamps <Preampstv@gmail.com>
2025-05-04 15:59:20 +0700

    edit script

---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemy;
+    public Transform[] spawnPoints;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnEnemy), 2, 1);
+    }
+
+    void SpawnEnemy()
+    {
+        if (enemy.Length == 0 || spawnPoints.Length == 0) return;
+
+        // เลือกศัตรูแบบสุ่ม
+        int enemyIndex = Random.Range(0, enemy.Length);
+
+        // เลือกจุด spawn แบบสุ่ม
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // สร้างศัตรู
+        Instantiate(enemy[enemyIndex], spawnPoints[spawnPointIndex].position, Quaternion.identity);
+    }
+}
