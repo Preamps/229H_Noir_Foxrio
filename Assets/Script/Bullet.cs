f65f29c86ab507630af882bbd77d49f59f4ffbdb
Preamps <Preampstv@gmail.com>
2025-05-04 17:54:18 +0700

    OnTriggerEnter2D

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy"))
         {
