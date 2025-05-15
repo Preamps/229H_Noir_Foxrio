3235c9011151593642ef8650055fcd24b82ec585
koztw <tanakrit.wana@bumail.net>
2025-05-15 09:59:48 +0700

    Levels
    lvl 1-3 added
    quit added

---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+   public void OpenLevel(int levelId)
+   {
+	   string levelname = "lvl " +	levelId;
+	   SceneManager.LoadScene(levelname);
+   }
+    public void QuitGame()
+   {
+	   Application.Quit();
+	   Debug.Log("Quit!");
+   }
+}
